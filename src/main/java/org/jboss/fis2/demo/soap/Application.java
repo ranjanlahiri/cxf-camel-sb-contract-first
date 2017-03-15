d5884e1c06d2cb0a4685e809f090756e27b421e7
Benjamin Holmes <benjamin.holmes@gmail.com>
2017-03-15 22:27:06 +0000

    refactored from base

--- src/main/java/org/jboss/fis2/demo/soap/Application.java
+++ src/main/java/org/jboss/fis2/demo/soap/Application.java
@@ -1,22 +1,16 @@
 
-package example.cxf_camel_sb_contract_first;
-
-//import org.apache.camel.component.servlet.CamelHttpTransportServlet;
-import org.apache.camel.builder.RouteBuilder;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
-
+package org.jboss.fis2.demo.soap;
 
 import javax.sql.DataSource;
 
-
 import org.apache.cxf.transport.servlet.CXFServlet;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 
 
@@ -25,24 +19,6 @@ import org.springframework.context.annotation.Bean;
  */
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
-/*
-public class Application extends RouteBuilder {
-
-    // must have a main method spring-boot can run
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void configure() throws Exception {
-        from("timer://foo?period=5000")
-            .setBody().constant("Hello World")
-            .log(">>> ${body}");
-    }
-}
-
-*/
-
 public class Application {
 
     /**
