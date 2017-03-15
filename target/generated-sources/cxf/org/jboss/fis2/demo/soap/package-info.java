3340acbd0a71e2a2c86819bdb051f0e79fe52f71
ranjanlahiri <ranjan.lahiri@gmail.com>
2017-03-15 15:18:26 +0000

    Removed target dir

--- target/generated-sources/cxf/org/jboss/fis2/demo/soap/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://soap.demo.fis2.jboss.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.jboss.fis2.demo.soap;
