a23429fa1f6c978939819a8243fc82c9ff3065b2
ranjanlahiri <ranjan.lahiri@gmail.com>
2017-03-16 16:17:37 +0000

    camel context updated as per recommendation from Ben

---
+++ target/generated-sources/cxf/org/jboss/fis2/demo/soap/EmployeeList.java
@@ -0,0 +1,69 @@
+
+package org.jboss.fis2.demo.soap;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for employeeList complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="employeeList"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="employeeList" type="{http://soap.demo.fis2.jboss.org/}employee" maxOccurs="unbounded" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "employeeList", propOrder = {
+    "employeeList"
+})
+public class EmployeeList {
+
+    @XmlElement(namespace = "", nillable = true)
+    protected List<Employee> employeeList;
+
+    /**
+     * Gets the value of the employeeList property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the employeeList property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getEmployeeList().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Employee }
+     * 
+     * 
+     */
+    public List<Employee> getEmployeeList() {
+        if (employeeList == null) {
+            employeeList = new ArrayList<Employee>();
+        }
+        return this.employeeList;
+    }
+
+}
